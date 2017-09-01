3b764d0145b58187d7d7478893a7875b7749b9ce
Leonel Gonzalez Vidales <leonelgv@gmail.com>
2017-09-01 11:25:21 -0500

    Programa de ejemplo que permite realizar la suma de n numeros. Para resolver este problema se realizaron dos algoritmos.

---
+++ src/ComplejidadAlgoritmica.java
@@ -0,0 +1,13 @@
+public class ComplejidadAlgoritmica {
+    public double calcularSumatoria(double n) {
+        double l = 0;
+        for(int a=0; a<n+1;a++) {
+            l = l + a;
+        }
+        return l;
+    }
+
+    public double calcularSuma(double n) {
+        return ((n*(n+1)) /2);
+    }
+}
---
+++ src/Programa.java
@@ -0,0 +1,7 @@
+public class Programa {
+    public static void main (String[] args) {
+        ComplejidadAlgoritmica algoritmica = new ComplejidadAlgoritmica();
+        System.out.println("La suma es: " + algoritmica.calcularSumatoria(100));
+        System.out.println("La suma es: " + algoritmica.calcularSuma(100));
+    }
+}
